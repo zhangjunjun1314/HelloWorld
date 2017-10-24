2824b5ec34db452a0aa80642d95d992e5b016541
zhangjunjun1314 <zxzdawe1314@163.com>
2017-10-24 16:56:51 +0800

    hello

---
+++ src/com/zjj/hello/HelloWorld.java
@@ -0,0 +1,17 @@
+package com.zjj.hello;
+
+/**
+ * Created by Administrator on 2017/8/23.
+ *
+ * 快捷键设置为Alt+/
+ * psvm :main函数
+ * sout：System.out.println()
+ *
+ */
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+
+    }
+}
